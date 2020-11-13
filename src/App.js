c558a6ad7eb4ad5640be6d454d3b695acc2c63e4
López Durán <dlopezd@geq.net>
2020-11-12 21:08:57 -0600

    Change H1 tag

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-    <h1>React application - This is my application</h1>
+        <h1>Under development :)</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
